e14638580cfc8891a7a893d0c8169552ff18ee7b
BenMenard <benjaminlmenard@gmail.com>
2024-05-23 17:00:24 -0400

    progressed further, added variables

---
+++ guess/target/debug/deps/guess-c9864acaf5573617.d
@@ -0,0 +1,5 @@
+/Users/bmenard/Documents/Code/rust_code/rust_projects/Guess_Bundle/guess/target/debug/deps/guess-c9864acaf5573617: src/main.rs
+
+/Users/bmenard/Documents/Code/rust_code/rust_projects/Guess_Bundle/guess/target/debug/deps/guess-c9864acaf5573617.d: src/main.rs
+
+src/main.rs:
---
+++ guess/target/debug/guess.d
@@ -0,0 +1,1 @@
+/Users/bmenard/Documents/Code/rust_code/rust_projects/Guess_Bundle/guess/target/debug/guess: /Users/bmenard/Documents/Code/rust_code/rust_projects/Guess_Bundle/guess/src/main.rs
